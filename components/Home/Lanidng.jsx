36880a110fe068a35d48ac7d58ea68bb2c4518c3
DevVatu <devvatu@gmail.com>
2021-09-20 16:23:36 +0200

    modified landing page, layout optimised for mobile

--- components/Home/Lanidng.jsx
+++ components/Home/Lanidng.jsx
@@ -5,14 +5,14 @@ function Lanidng() {
   return (
     <div className={styles.landing}>
       <div className={styles.img_container}>
-        <Image src="/imgs/ananas.png" layout="fill" />
+        <Image src="/imgs/ananas.png" layout="fill" objectFit="cover" />
       </div>
       <article>
         <h1>Készen állsz a partira?!</h1>
-        <h2>
+        <p>
           Fedezd fel az alkalomhoz illő parti kellékeinket! Itt mindent
           megtalálsz.
-        </h2>
+        </p>
         <button className="btn">IRÁNY A BOLT</button>
       </article>
     </div>
